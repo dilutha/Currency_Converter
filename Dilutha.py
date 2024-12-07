61f6ac3976fbf2b2eeef60a8538da89e9cee8786
dilutha <67290268+dilutha@users.noreply.github.com>
2024-12-07 13:36:52 +0530

    Create Dilutha.py
    Import necessary Libraries and connect to ExchangeRate-APi

---
+++ Dilutha.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import requests
+import pandas as pd
+import plotly.express as px
+API_KEY = '6ab4dd50f1659904149c169e'  # Use your provided API key
+BASE_URL = 'https://v6.exchangerate-api.com/v6/{}/'
